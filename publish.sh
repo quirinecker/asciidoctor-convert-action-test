6d77c02ce3e1c7dec9958ff0a454625518920606
QuirinEcker <quirin.ecker@bajupa.com>
2021-10-24 11:11:07 +0200

    updating github pages

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+./convert.sh $1 $2 $3
+
+remote=$(git remote get-url origin)
+cd "$OUTPUTPATH" || exit
+
+rm -rf .git
+
+git init
+
+git add .
+
+git commit -m "updating github pages"
+
+git branch gh-pages
+
+git remote add origin "$remote"
+
+git push -f origin gh-pages
